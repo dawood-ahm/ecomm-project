27ab8b4285a4f7e0735a80693b9ba18b8bc3d087
dawood ahmed <dawood.bhatti@gmail.com>
2023-06-06 20:27:29 +0500

    ecomm full project

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,19 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/logout', function () {
+    Session::forget('user');
+    return redirect('login');
+});
+Route::view('/register', 'register');
 Route::post('/login',[UserController::class,'login']);
+Route::post('/register',[UserController::class,'register']);
 Route::get('/',[ProductController::class,'index']);
+Route::get('/detail/{id}',[ProductController::class,'detail']);
+Route::get("search",[ProductController::class,'search']);
+Route::post("add_to_cart",[ProductController::class,'addtocart']);
+Route::get("cartlist",[ProductController::class,'cartlist']);
+Route::get("/removecart/{id}",[ProductController::class,'removeItem']);
+Route::get("ordernow",[ProductController::class,'ordernow']);
+Route::post("orderplace",[ProductController::class,'orderPlace']);
+Route::get("myorders",[ProductController::class,'myOrders']);
